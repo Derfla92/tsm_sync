63aa4abe49b3a6d8ad72743e3c597a274cd8032c
Alfred Spjut Molin <alfsp550@student.liu.se>
2022-05-08 01:42:48 +0200

    funkar

--- tsm_sync.py
+++ tsm_sync.py
@@ -1,17 +1,23 @@
 #! /usr/bin/env python3
 
+from encodings import utf_8
 from socket import *
 
 
-addr = ("",8081)
+addr = ("0.0.0.0",8080)
 
 serversocket = socket(AF_INET,SOCK_STREAM)
 #serversocket.setsockopt(SOL_SOCKET, SO_REUSEADDR,1)
 server = serversocket.bind(addr)
+serversocket.listen()
+fd, address = serversocket.accept()
+print(address)
 while True:
-    serversocket.listen()
 
-    fd, address = serversocket.accept()
 
-    print(fd)
-    print(address)
+
+    recv = fd.recv(512)
+
+    print(recv.decode("UTF-8"))
+fd.close()
+    
